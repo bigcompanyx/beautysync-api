16ec8d1e2383f726c3a3fae335592a21bff6405f
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2023-11-05 22:24:25 +0200

    Decrease number of fake Users

--- src/Story/DefaultUsersStory.php
+++ src/Story/DefaultUsersStory.php
@@ -9,6 +9,6 @@ final class DefaultUsersStory extends Story
 {
     public function build(): void
     {
-        UserFactory::new()->createMany(20);
+        UserFactory::new()->createMany(10);
     }
 }
