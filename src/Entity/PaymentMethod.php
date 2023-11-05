825c8bf9d90ee054b8fa2e867c25c58b77529092
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2023-11-05 22:46:44 +0200

    Add PaymentMethod entity

---
+++ src/Entity/PaymentMethod.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaymentMethodRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaymentMethodRepository::class)]
+class PaymentMethod
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
