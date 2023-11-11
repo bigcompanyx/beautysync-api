11b6d0de633faaa38c5d4cc4ae437738725957c2
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2023-11-11 12:29:10 +0200

    Add assertSame & assertResponseHeaderSame for testUserSubscriptionGet test

--- tests/Api/UserSubscriptionTest.php
+++ tests/Api/UserSubscriptionTest.php
@@ -21,9 +21,20 @@ class UserSubscriptionTest extends ApiTestCase
     }
 
     public function testUserSubscriptionGet() {
-        static::createClient()->request('GET', self::API_ENDPOINT.'/1');
+        $response = static::createClient()->request('GET', self::API_ENDPOINT.'/1')->toArray();
 
         $this->assertResponseIsSuccessful();
+
+        $this->assertResponseHeaderSame('content-type', 'application/json; charset=utf-8');
+
+        $this->assertSame([
+            'id',
+            'subscriptionPlan',
+            'expirationDate',
+            'status',
+            'trialActiveTrialActive',
+            'user'
+        ], array_keys($response));
     }
     
     public function testUserSubscriptionDelete() {
