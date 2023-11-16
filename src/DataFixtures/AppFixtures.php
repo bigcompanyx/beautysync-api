061d25252f3d37c5ed7f680a27af2007d24e11cb
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2023-11-16 19:23:51 +0200

    Add CompanyFactory & DefaultCompanyStory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Story\DefaultBookingStory;
 use App\Story\DefaultClientStory;
+use App\Story\DefaultCompanyStory;
 use App\Story\DefaultUsersStory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -15,5 +16,6 @@ class AppFixtures extends Fixture
         DefaultUsersStory::load();
         DefaultBookingStory::load();
         DefaultClientStory::load();
+        DefaultCompanyStory::load();
     }
 }
