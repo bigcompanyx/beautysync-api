13f51e057b91b1b66f4469c6619482e58cbe5aff
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2023-11-17 14:42:36 +0200

    Add propertyAccessor to getRequestBody method

--- tests/ImprovedApiTestCase.php
+++ tests/ImprovedApiTestCase.php
@@ -2,23 +2,35 @@
 
 namespace App\Tests;
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use function Symfony\Component\String\u;
 
 class ImprovedApiTestCase extends ApiTestCase
 {
+    private $propertyAccessor;
+
     public function __construct()
     {
+        $this->propertyAccessor = PropertyAccess::createPropertyAccessor();
+
         parent::__construct();
     }
 
     public function getRequestBody(array $requestParams, object $DTO): array
     {
+
         if (($key = array_search('id', $requestParams)) !== false) {
             unset($requestParams[$key]);
         }
 
         $requestBody = [];
         array_walk($requestParams, function($param) use(&$requestBody, $DTO){
-            $requestBody[$param] = $DTO->{'get'.ucfirst($param)}();
+            $propertyName = u($param)->snake();
+            $value = $this->propertyAccessor->getValue($DTO, $propertyName);
+            if($value instanceof \DateTimeInterface){
+                $value = $value->format('Y-m-d H:i:s');
+            }
+            $requestBody[$param] = $value;
         });
 
         return $requestBody;
